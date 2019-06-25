531132bb5d9f927567d3f2a6741b016209cd5174
pytimer <lixin20101023@gmail.com>
2019-06-25 14:30:53 +0800

    add k8s client

--- pkg/kube/kube.go
+++ pkg/kube/kube.go
@@ -1,1 +1,31 @@
 package kube
+
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+// NewClient creates k8s client
+func NewClient(kubeConfigFile string) (kubernetes.Interface, error) {
+	var clientConfig *rest.Config
+	var err error
+	if len(kubeConfigFile) > 0 {
+		clientConfig, err = clientcmd.BuildConfigFromFlags("", kubeConfigFile)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		clientConfig, err = rest.InClusterConfig()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	client, err := kubernetes.NewForConfig(clientConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
